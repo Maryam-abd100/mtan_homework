95e16dbc9d6ca42d5fc3a12a617ec276722ab984
Maryam <maryabd002678@gmail.com>
2023-01-08 00:36:19 +0300

    add file to save tasks

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# this file is main file and all file is include and import here
-#     work:  
-# 1- load file 
-# 2- ask user if he need to show data or add data { if he want to show data show them and return to 2}
-# 3- to add data make hime input time and data and check if time is exsit {if exit ask him befor to edite them} 
-# 4- sort data by time
-# 5- edit file and return to 2 or 1 
 print("Hello")
