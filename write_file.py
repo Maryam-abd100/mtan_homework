a18f36d93fdf8b259752d0f6e04c5ba4e62a036f
Maryam <maryabd002678@gmail.com>
2023-01-08 11:00:26 +0300

    create function and open file

--- write_file.py
+++ write_file.py
@@ -3,3 +3,17 @@
 #return void 
 #par (array time,array task)
 # i am workin on this file
+def write_file(date,data):
+     add_Task=open("Tasks.txt","a")
+     add_Task.write("2023/")
+     add_Task.write(str(date[1]))
+     add_Task.write("/")
+     add_Task.write(str(date[0]))
+     add_Task.write("\t\t\t\t")
+     add_Task.write(data)
+     add_Task.write("\n")
+     add_Task.close()
+     
+write_file([16,12],"study")
+write_file([3,5],"meeting with company")
+write_file([22,7],"sport")
