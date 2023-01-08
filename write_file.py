0f2382bf51b8a0ba6c45a9b9c47cfe083fb61d33
Maryam <maryabd002678@gmail.com>
2023-01-09 00:43:28 +0300

    last change (finished)

--- write_file.py
+++ write_file.py
@@ -5,14 +5,11 @@
 # i am workin on this file
 def write_file(date,data):
      d=[]
-     i=len(d)
+     i=len(d)   
      d.insert(i,date)
      add_Task=open("Tasks.txt","a")
      add_Task.write(str(d[i]))
      add_Task.write("\n\t\t\t\t")
      add_Task.write(data)
      add_Task.write("\n")
-     i=i+1
-     if(i==7):
-          i=0
-     add_Task.close()      
+     add_Task.close()     
