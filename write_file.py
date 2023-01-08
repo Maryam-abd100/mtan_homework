e734bd8f7aed4f0cb127733919c50f24d3f15dff
Maryam <maryabd002678@gmail.com>
2023-01-08 15:51:21 +0300

    correct code

--- write_file.py
+++ write_file.py
@@ -4,12 +4,17 @@
 #par (array time,array task)
 # i am workin on this file
 def write_file(date,data):
+     d=[]
+     i=len(d)
+     d.insert(i,date)
      add_Task=open("Tasks.txt","a")
-     add_Task.write("2023/")
-     add_Task.write(str(date[1]))
-     add_Task.write("/")
-     add_Task.write(str(date[0]))
+     add_Task.write(str(d[i]))
      add_Task.write("\t\t\t\t")
      add_Task.write(data)
      add_Task.write("\n")
+     i=i+1
+     if(i==7):
+          i=0
      add_Task.close()   
+
+write_file(5,"studt")     
