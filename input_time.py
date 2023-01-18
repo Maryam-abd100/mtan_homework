3da9d40272df51de610278841c076da7fd88b69a
Maryam <maryabd002678@gmail.com>
2023-01-18 22:04:45 +0300

    finish code

--- input_time.py
+++ input_time.py
@@ -3,3 +3,16 @@
 #return 1=>if ok do that ,0=>time is exsit ask them to befor edite it 
 #par no parymater
 #function use check_time,read_file,write_file
+import write_file
+import check_time
+def input_time():
+     print("TIME : \n")
+     time=input()
+     if(check_time(int(time))==1):
+         print("THE TIME IS EXIST ....please edit time ")
+         return 0
+     else:    
+         print("YOUR TASK : \n")
+         task=input()
+         write_file(time,task)
+         return 1
