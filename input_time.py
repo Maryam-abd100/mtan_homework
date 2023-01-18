8d2cbc9bf2b7b33ff0196072c72767ea1f3f067a
Maryam <maryabd002678@gmail.com>
2023-01-18 22:06:57 +0300

    I work in this file

--- input_time.py
+++ input_time.py
@@ -2,4 +2,4 @@
 #work input time and task from user
 #return 1=>if ok do that ,0=>time is exsit ask them to befor edite it 
 #par no parymater
-#function use check_time,read_file,write_file
+#function use check_time,read_file,write_file .....I work in this
